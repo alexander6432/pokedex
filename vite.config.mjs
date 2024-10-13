c2eaabc020bda4fd70aaf5ebaa7a5ffae9a5f083
Alexander Gallardo <alexander6432@gmail.com>
2024-10-12 20:14:34 -0600

    preparando deploy

--- vite.config.mjs
+++ vite.config.mjs
@@ -10,7 +10,7 @@ import { fileURLToPath, URL } from "node:url";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/pokedex/",
+  base: "https://alexander6432.github.io/pokedex/",
 
   plugins: [
     Vue({
